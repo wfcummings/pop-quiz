7d080f0bc3f88ae3bec93af13ae610a81462aa3f
willcummings <will.cummings2@gmail.com>
2026-09-15 15:56:28 -0400

    Add last question and conditionals to all

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 alert("Welcome to the NFL quiz!");
 alert("There are 3 multiple choice questions. Choose 1 or 2. Goodluck!");
 
-let score;
+let score = 0;
+let totalquestions = 3;
 
-const answer1 = prompt(
-  "What city are the Jaguars located in?\n1) Houston \n2) Jacksonville",
-);
+const answer1 = prompt(`What city are the Jaguars located in?
+  [1] Houston
+  [2] Jacksonville,
+`);
 
 const selection1 = Number(answer1);
-///////
-const answer2 = prompt(
-  "What is the mascot of the NFL team located in Dallas?\n1) Steelers \n2) Cowboys",
-);
-const selection2 = Number(answer2);
-/////
-/*
-if (answer1 === "2") {
-  alert("Correct");
+
+if (Number.isNaN(selection1)) {
+  alert("You must enter a number.");
+  exit(answer1);
+} else if (selection1 < 1 || selection1 > 2) {
+  alert("Your answer must be either number 1 or 2.");
+  exit(answer1);
+} else if (selection1 === 2) {
+  score += 1;
 }
-*/
+
+///////
+const answer2 = prompt(`What is the mascot of the NFL team located in Dallas?
+  [1] Steelers
+  [2] Cowboys
+`);
+
+const selection2 = Number(answer2);
+
+if (Number.isNaN(selection2)) {
+  alert("You must enter a number.");
+} else if (selection2 < 1 || selection2 > 2) {
+  alert("Your answer must be either number 1 or 2.");
+} else if (selection2 === 2) {
+  score += 1;
+}
+
+/////
+const answer3 = prompt(
+  "Who is the quarterback of the Philadelphia Eagles?\n1)Jalen Hurts \n2) Tom Brady",
+);
+const selection3 = Number(answer3);
+
+if (Number.isNaN(selection3)) {
+  alert("You must enter a number.");
+} else if (selection3 < 1 || selection3 > 2) {
+  alert("Your answer must be either number 1 or 2.");
+} else if (selection3 === 2) {
+  score += 1;
+}
