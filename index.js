d8a3b962320a996ae7895f14aca6472667a70f7d
willcummings <will.cummings2@gmail.com>
2026-09-15 14:18:07 -0400

    Added questions 1 and 2

---
+++ index.js
@@ -0,0 +1,21 @@
+alert("Welcome to the NFL quiz!");
+alert("There are 3 multiple choice questions. Choose 1 or 2. Goodluck!");
+
+let score;
+
+const answer1 = prompt(
+  "What city are the Jaguars located in?\n1) Houston \n2) Jacksonville",
+);
+
+const selection1 = Number(answer1);
+///////
+const answer2 = prompt(
+  "What is the mascot of the NFL team located in Dallas?\n1) Steelers \n2) Cowboys",
+);
+const selection2 = Number(answer2);
+/////
+/*
+if (answer1 === "2") {
+  alert("Correct");
+}
+*/
